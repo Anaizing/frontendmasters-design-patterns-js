982eb3b98228ec26395a175fd762964a2a1bd73c
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2024-08-22 00:33:26 +1000

    add varuiable names to html, create a interpolate function and call that function that passes the template.innerHTML and the object with the variable values

--- CoffeeMasters/initial/components/CartItem.js
+++ CoffeeMasters/initial/components/CartItem.js
@@ -10,17 +10,25 @@ export default class CartItem extends HTMLElement {
         this.innerHTML = ""; // Clear the element
 
         const template = document.getElementById("cart-item-template");
-        const content = template.content.cloneNode(true);
 
-        this.appendChild(content);    
 
-        this.querySelector(".qty").textContent = `${item.quantity}x`;
-        this.querySelector(".name").textContent = item.product.name;
-        this.querySelector(".price").textContent = `$${item.product.price.toFixed(2)}`;
+        function interpolate(str, params) {
+            let names = Object.keys(params);
+            let values = Object.values(params); 
+            const body = `return \`${str}\`;`;
+            return new Function(...names, body)(...values);
+        }
+
+        this.innerHTML = interpolate(template.innerHTML, {
+            qty: `${item.qty}x`,
+            price: `$${item.product.price.toFixed(2)}`,
+            name: item.product.name
+        })
+
         this.querySelector("a.delete-button").addEventListener("click", event => {
             removeFromCart(item.product.id);
         })
-      }
+    }
 }
 
 customElements.define("cart-item", CartItem);
